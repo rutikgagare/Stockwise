fcda17a0d3a3c009244cbe236d107eba5577bace
rutikgagare <rutikgagare091971@gmail.com>
2024-03-04 10:07:50 +0530

    Update home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import classes from './Home.module.css';
+import logisticsImage from '../Images/logistics.png'
 
 const Home = () => {
 
   return (
-    <div className={classes.main}>
-      <h2>Streamline Your Inventory, Simplify Your Business</h2>
-      <p>
-        Welcome to Stockwise, your comprehensive inventory management solution. Stockwise is a Software as a Service (SaaS) application built on the MERN (MongoDB, Express.js, React.js, Node.js) stack. It empowers organizations to efficiently manage their inventory, streamline stock tracking, record sales transactions, and analyze sales and profit data with ease.
-      </p>
+    <div className={classes.home}>
+
+      <div className={classes.left}>
+        <img src={logisticsImage} alt="" />
+      </div>
+
+      <div className={classes.right}>
+        <h2>Streamline Your Inventory, Simplify Your Business</h2>
+        <p>
+          Welcome to Stockwise, your comprehensive inventory management solution. Stockwise is a Software as a Service (SaaS) application built on the MERN (MongoDB, Express.js, React.js, Node.js) stack. It empowers organizations to efficiently manage their inventory, streamline stock tracking, record sales transactions, and analyze sales and profit data with ease.
+        </p>
+      </div>
     </div>
   )
 }
