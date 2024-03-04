a00f9ae520d5cfce90e54020cfdf377c77abc840
Siddhesh Pardeshi <siddheshpardeshi0@gmail.com>
2024-03-04 10:36:54 +0530

    improved place orders page ui

--- frontend/src/pages/PlaceOrderPage.js
+++ frontend/src/pages/PlaceOrderPage.js
@@ -20,68 +20,68 @@ const PlaceOrderPage = () => {
 
                 <div className={classes.right}>
                     <h1>Place Order</h1>
-
-                    {orders.map((order, idx) => (
-                        <div className={classes.order_form} key={idx}>
-                            Product Name:
-                            <input
-                                type="text"
-                                value={order?.name}
-                                onChange={(e) => {
-                                    const newOrders = [...orders];
-                                    newOrders[idx] = { ...order, name: e.target.value };
-                                    setOrders(newOrders);
-                                }}
-                            />
-                            Product Vendor:
-                            <input
-                                type="text"
-                                value={order?.vendor}
-                                onChange={(e) => {
-                                    const newOrders = [...orders];
-                                    newOrders[idx] = { ...order, vendor: e.target.value };
-                                    setOrders(newOrders);
-                                }}
-                            />
-                            Vendor Email:
-                            <input
-                                type="email"
-                                value={order?.email}
-                                onChange={(e) => {
-                                    const newOrders = [...orders];
-                                    newOrders[idx] = { ...order, email: e.target.value };
-                                    setOrders(newOrders);
-                                }}
-                            />
-                            Quntity:
-                            <input
-                                type="number"
-                                min={1}
-                                max={1000}
-                                value={order?.quantity}
-                                onChange={(e) => {
-                                    const newOrders = [...orders];
-                                    newOrders[idx] = { ...order, quantity: e.target.value };
-                                    setOrders(newOrders);
-                                }}
-                            />
-                            <button
-                                onClick={() => {
-                                    const newOrders = orders.filter((_, i) => i !== idx);
-                                    setOrders(newOrders);
-                                }}
-                            >
-                                Remove
-                            </button>
-                        </div>
-                    ))}
-
+                    <div className={classes.orders}>
+                        {orders.map((order, idx) => (
+                            <div className={classes.order_form} key={idx}>
+                                Product Name:
+                                <input
+                                    type="text"
+                                    value={order?.name}
+                                    onChange={(e) => {
+                                        const newOrders = [...orders];
+                                        newOrders[idx] = { ...order, name: e.target.value };
+                                        setOrders(newOrders);
+                                    }}
+                                />
+                                Product Vendor:
+                                <input
+                                    type="text"
+                                    value={order?.vendor}
+                                    onChange={(e) => {
+                                        const newOrders = [...orders];
+                                        newOrders[idx] = { ...order, vendor: e.target.value };
+                                        setOrders(newOrders);
+                                    }}
+                                />
+                                Vendor Email:
+                                <input
+                                    type="email"
+                                    value={order?.email}
+                                    onChange={(e) => {
+                                        const newOrders = [...orders];
+                                        newOrders[idx] = { ...order, email: e.target.value };
+                                        setOrders(newOrders);
+                                    }}
+                                />
+                                Quntity:
+                                <input
+                                    type="number"
+                                    min={1}
+                                    max={1000}
+                                    value={order?.quantity}
+                                    onChange={(e) => {
+                                        const newOrders = [...orders];
+                                        newOrders[idx] = { ...order, quantity: e.target.value };
+                                        setOrders(newOrders);
+                                    }}
+                                />
+                                <button
+                                    onClick={() => {
+                                        const newOrders = orders.filter((_, i) => i !== idx);
+                                        setOrders(newOrders);
+                                    }}
+                                >
+                                    Remove
+                                </button>
+                            </div>
+                        ))}
+                    </div>
                     <div className={classes.action_buttons} >
                         <button className={classes.add_btn} onClick={() => { setOrders([...orders, {}]) }}>
                             Add
                         </button>
 
-                        <button className={classes.clear_btn} onClick={ handleClearAll }>
+                        <button className={classes.clear_btn} onClick={handleClearAll}>
                             Remove All
                         </button>
 
