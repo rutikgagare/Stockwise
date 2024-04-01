a4ba1021b54040f087602df0f9fa0cb321b1522c
Siddhesh Pardeshi <siddheshpardeshi0@gmail.com>
2024-04-01 11:40:21 +0530

    routine

--- backend/tests/orderController.test.js
+++ backend/tests/orderController.test.js
@@ -6,12 +6,42 @@ const app = require('../index');
 chai.use(chaiHttp);
 const expect = chai.expect;
 
+const Organization = require("../models/organizationModel")
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to the database…"))
+  .catch((err) => console.error("Connection error:", err));
+
+
 describe("POST /order/orders", () => {
     it("should should return a list of all the orders given by an organization", (done) => {
         chai.request(app)
             .post("/order/orders")
             .send({
                 orgId: "65f316a1991edde66ee55fc1" // get orgId dynamically...
+            })
+            .end((err, res) => {
+                expect(res.status).to.be.equal(200);
+                // more assertions goes here...
+                // ...
+            })
+            
+        done();
+    })
+
+});
+
+describe("POST /order/create", () => {
+    it("should should return a list of all the orders given by an organization", async (done) => {
+        const org = Organization.findOne();
+        const admin = User.findOne();
+        chai.request(app)
+            .post("/order/create")
+            .send({
+                org, admin, cart
             })
             .end((err, res) => {
                 expect(res.status).to.be.equal(200);
